4dadac8c5f41054643e62301b9e40fafe87ad674
Manish Kumar <manish.jangid08@gmail.com>
2019-10-08 10:05:52 +0530

    Longest substring without repeating char with hashmap, ascii char set

--- Problem2.cpp
+++ Problem2.cpp
@@ -32,7 +32,11 @@ Explanation: The answer is "wke", with the length of 3.
 
  * Solution 2: Using two pointer approach 
  * Time complexity :O(n)
-  * Space complexity : O(n) using hash set to keep track of duplicates 
+  * Space complexity : O(1) using hash set to keep track of duplicates . max 255 characters 
+
+  * Solution 3: Using hash map. It avoids checking windows like pwwke --> ww window is skipped in this method 
+ * Time complexity :O(n)
+  * Space complexity : O(1) as hash map can contain max 255 ASCII char set 
  *
  */
 
@@ -150,6 +154,64 @@ public:
             }
         }
 
+        return fin_len;
+    }
+
+    /* Using hash maps to store the latest index at which the char is found. Note that the latest index is 1 index based
+       instead of 0 based.
+       1. If the char is already in the hmap, move your start pointer to the max of existing start or the current char end
+       2. FOr eg : abba is the case where we have a problem if we dont use the latest start.
+       3. Now, also for each iteration update the index for the character and then check if we have found a new max length.
+       4. Keep on iterating unless you reach the end of string.
+
+       Time is 20ms for the following. 
+     */
+    int lengthOfLongestSubstring_ver3(string s) {
+        int start = 0, end = 0;
+        int fin_len = 0;
+        int st_len = s.length();
+        
+        if (!st_len) {
+            return fin_len;
+        }
+        
+        unordered_map<char, int> hmap;
+        while (end < st_len) {
+            if (hmap.find(s[end]) != hmap.end()) {
+                start = max(start,hmap[s[end]]);
+            }
+            hmap[s[end]] = end + 1;
+            fin_len = max(fin_len, (end - start) + 1);
+            end++;
+        }
+        
+        return fin_len;
+    }
+
+    /* Further exection time can be reduced by taking ascii char set and avoid overhead of maintaing a hash map */
+    int lengthOfLongestSubstring_ver4(string s) {
+        int start = 0, end = 0;
+        int arr[255];
+        int fin_len = 0;
+        int st_len = s.length();
+        
+        if (!st_len) {
+            return fin_len;
+        }
+        
+        for (int idx = 0; idx < 255; idx++) {
+            arr[idx] = 0;
+        }
+        
+        while (end < st_len) {
+            if (arr[s[end]] != 0) {
+                start = max(start,arr[s[end]]);
+            }
+            arr[s[end]] = end + 1;
+            fin_len = max(fin_len, (end - start) + 1);
+            end++;
+        }
+        
         return fin_len;
     }
 };
