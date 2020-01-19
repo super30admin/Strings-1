6374f5969061ea3f699a3cb4defda1b01f78f085
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2020-01-19 11:47:26 -0800

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,32 @@
+// Time Complexity - O(m+n) where m is the length of T and n is the length of S
+// Space Complexity - O(1) since the maximum space used by the hashmap will be for 26 characters
+// In this problem to find the 
+
+class Solution {
+    public String customSortString(String S, String T) {
+        StringBuilder sb = new StringBuilder();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c : T.toCharArray()){
+            map.put(c,map.getOrDefault(c,0) + 1);
+        }
+        for(char c : S.toCharArray()){
+            if(map.containsKey(c)){
+                int count = map.get(c);
+                while(count > 0){
+                    sb.append(c);
+                    count--;
+                }
+                map.remove(c);
+            }
+        }
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            int count = entry.getValue();
+            char c = entry.getKey();
+            while(count > 0){
+                sb.append(c);
+                count--;
+            }            
+        }
+        return sb.toString();
+    }
+}
