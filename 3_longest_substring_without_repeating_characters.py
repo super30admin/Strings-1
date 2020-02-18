e30da3f1ffcf9ca28a90ba30f03553e1b5757662
Nikhil Agrawal <nikrasiya@gmail.com>
2020-02-17 23:54:27 -0500

    all problems complete

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -1,9 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
+            https://leetcode.com/problems/longest-substring-without-repeating-characters/
             Time Complexity - O(n)
             Space Complexity - O(n)
             'n' is the length of the string 's'
+            Explanation -
+                - If the current char is already in dictionary, it means
+                  we cannot extend the string further, start a new string
+                  from one position ahead of the cur char.
+                  # Edge case - We need to make sure slow pointer does not move back
+                - The difference between fast and slow + 1 is the cur non repeating string.
+                  Keep track of maximum using a variable
         """
         slow = fast = max_len = 0
         pos_dic = {}
@@ -34,6 +42,10 @@ class Solution:
 
 if __name__ == '__main__':
     print(Solution().lengthOfLongestSubstring('abcabcbb'))
+    # edge cases
+    # when we are at the second a, if we don't take max(slow,a:1)
+    # we would have moved slow to 1st index.
+    print(Solution().lengthOfLongestSubstring('abba'))
     print(Solution().lengthOfLongestSubstring('abbabc'))
     print(Solution().lengthOfLongestSubstring('bbbbb'))
     print(Solution().lengthOfLongestSubstring('pwwkew'))
