a659b1e10199eecfb0b0d497abac985f70ada6c8
Debanjana Dasgupta <debanjana_ddg@yahoo.co.in>
2020-05-08 19:31:26 -0500

    Problem 1 solved

---
+++ Pronlem1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String customSortString(String S, String T) {
+        StringBuilder sb = new StringBuilder();
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(Character c: T.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        
+        for(Character c: S.toCharArray()){
+            if(map.containsKey(c)){
+                int count = map.get(c);
+                while(count > 0){
+                    sb.append(c);
+                    count--;
+                }
+                map.remove(c);
+            }
+        }
+        
+        for(Map.Entry<Character, Integer> entry: map.entrySet()){
+            int count = entry.getValue();
+            Character c = entry.getKey();
+            while(count > 0){
+                sb.append(c);
+                count--;
+            }
+        }
+        return sb.toString();
+    }
+}
