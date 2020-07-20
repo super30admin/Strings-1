c1e09c26c79ddfe23ccebad80da654308135c920
chirag <cr.tagadiya@gmail.com>
2020-07-19 22:57:35 -0400

    Longest non repeating substring

--- Longest_NonRepeating_Substring.java
+++ Longest_NonRepeating_Substring.java
@@ -27,3 +27,60 @@ public class Longest_NonRepeating_Substring{
         return  max_upto_date;
     }
 }
+
+//
+
+/*
+
+public int lengthOfLongestSubstring(String s) {
+        if (s == null  || s.length() == 0 ) return 0;
+        
+        int low = 0;
+        int high = 0;
+        int [] index = new int[128];
+        int max_upto_date = Integer.MIN_VALUE;
+        // Map<Character, Integer> map = new HashMap();
+        int n = s.length();
+        while(high < n){
+            Character c = s.charAt(high);
+//          set low to the place where last index of low
+            low = Math.max(low, index[c]);
+//             get max_upto_date
+            max_upto_date = Math.max(max_upto_date, high - low + 1);
+//             set index of char to high+1 next time when we get this char we need this index to move
+            
+            index[c] =  high+1;
+            high++;
+        }
+        return max_upto_date;
+    }
+
+*/
+
+/*
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null  || s.length() == 0 ) return 0;
+        
+        int low = 0;
+        int high = 0;
+        int max_upto_date = Integer.MIN_VALUE;
+        Map<Character, Integer> map = new HashMap();
+        // "abcdeabcedgx"
+        int n = s.length();
+        while(high < n){
+            
+            Character c = s.charAt(high);
+            // System.out.println("low :"+low);
+            if(map.containsKey(c)){
+                low =  Math.max(map.get(c), low);
+            }
+            max_upto_date = Math.max(max_upto_date, high - low + 1);
+            
+            map.put(c, high+1);
+        
+            high++;
+        }
+        return max_upto_date;
+    }
+
+*/
