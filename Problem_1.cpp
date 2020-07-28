2daa8cb429855e68f016bb9d41a7ec8670048916
Abhinav R <arameshb@buffalo.edu>
2020-07-28 17:25:34 +0530

    Strings-1 Done.

---
+++ Problem_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string customSortString(string S, string T) {
+        int count[26] = {0};
+        string result = "";
+        for(char c: T){
+            count[c-'a'] += 1;
+        }
+        
+        for(char c: S){
+            for(int k=0; k<count[c-'a']; k++){
+                result += c;
+            }
+            count[c-'a'] = 0;
+        }
+        
+        for(char c = 'a'; c<='z'; c++){
+            for (int i = 0; i < count[c - 'a']; i++){
+                result += c;
+            }
+        }
+        return result;
+    }
+};
