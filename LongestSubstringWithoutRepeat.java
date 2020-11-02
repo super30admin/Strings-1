0264eb51e0bfd2c37d1bd0c7f7d1d393804c6a05
Dhruv <jawalkad@oregonstate.edu>
2020-11-02 11:42:03 -0800

    longest substring

--- LongestSubstringWithoutRepeat.java
+++ LongestSubstringWithoutRepeat.java
@@ -1,35 +1,30 @@
-// Time Complexity : O(n^3)
+// Time Complexity : O(n)
 // Space Complexity : O(n)
-// Did this code successfully run on Leetcode : 986/987, TLE
-// Any problem you faced while coding this : no, bruteforce
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 // Your code here along with comments explaining your approach
-// Generate all possible substrings
-// validate if substring doesn't contain repeat chars, update max substring length
+// use sliding window approach by starting from i=0 find max substring length starting from index i, add chars to set
+// max substring length == set size, if we encounter duplicate, move i
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if(s==null || s.length()==0) return 0;
         
-        int maxSubStringLength = 0;
-        for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(validSubString(s, i, j)){
-                    maxSubStringLength = Math.max(maxSubStringLength, j-i+1);
-                }
+        int n = s.length();
+        HashSet<Character> set = new HashSet<>();
+        int ans=0, i=0, j=0;
+        
+        while(i<n && j<n){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j-i);
+            }
+            else{
+                set.remove(s.charAt(i++));
             }
         }
         
-        return maxSubStringLength;
-    }
-    
-    private boolean validSubString(String s, int i, int j){
-        Set<Character> set = new HashSet();
-        
-        for(int k=i; k<=j; k++){
-            set.add(s.charAt(k));
-        }
-        
-        return set.size()==(j-i+1);
+        return ans;
     }
 }
