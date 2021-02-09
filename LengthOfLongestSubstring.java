af46dea78aab05d64c24d3672e6e2b76a38ef4d9
Nilesh Deokar <76528900+nilesh-525@users.noreply.github.com>
2021-02-09 14:39:08 -0500

    Update LengthOfLongestSubstring.java

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -1,4 +1,20 @@
+/*
+    Time : O(N^2) | After each repeated char for loop starts from beginning so. (Not 100% sure)
+    Space : O(N) | N being length of s. HashMap space
+    Leetcode : YES
+*/
+
+
 class LengthOfLongestSubstring {
+/*
+    Approach :
+    1. Iterate through given string while maintaining hashmap of char and indexes.
+    2. If we encounter char which is already present in the hashmap. then reset i 
+       to the index of that char+1
+    3. maintain curr and max variables and update curr by 1 when you add new element to substring.
+    4. reset curr = 0 when you encounter repeated char.
+*/
+    
     public int lengthOfLongestSubstring(String s) {
         if(s == null ) return 0;
         if(s.length() <= 1) return s.length();
