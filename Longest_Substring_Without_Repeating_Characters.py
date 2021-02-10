47ddfbd1947a48ec43fd6562a60a0a3f1833684e
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-02-09 18:56:01 -0600

    Update Longest_Substring_Without_Repeating_Characters.py

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -5,7 +5,7 @@
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-		# Base case
+	# Base case
         if not s:
             return 0
         
@@ -16,13 +16,13 @@ class Solution(object):
 		
         for i in range(len(s)):
             if s[i] in dic:
-				# since need to avoid going back, we will be traveling back only when the slow pointer is smaller 
-				# than current pointer value in the dic
+		# since need to avoid going back, we will be traveling back only when the slow pointer is smaller 
+		# than current pointer value in the dic
                 slow = max(slow, dic[s[i]])
-			# why i + 1 since we need to pass repeating charcter
+	    # why i + 1 since we need to pass repeating charcter
             dic[s[i]] = i + 1
 			
-			# Check maximum length of substring everytime
+	    # Check maximum length of substring everytime
             maximum = max(maximum, i - slow + 1)
             
         return maximum
