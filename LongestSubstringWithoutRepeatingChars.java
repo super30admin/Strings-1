dda50307bba308592153c393a1dae74f6b13aec6
omkar <omkarvaidya13@gmail.com>
2021-04-06 08:35:32 -0400

    Added optimized solution

--- LongestSubstringWithoutRepeatingChars.java
+++ LongestSubstringWithoutRepeatingChars.java
@@ -40,3 +40,38 @@ class LongestSubstringWithoutRepeatingChars {
         return result;
     }
 }
+
+// Optimized Sliding Window Solution: 
+
+class LongestSubstringWithoutRepeatingChars {
+
+    // Time Complexity: O(n)    (where n -> length of the string)
+    // Space Complexity: O(n)
+
+    public int lengthOfLongestSubstring(String s) {
+        // Edge Case Checking
+        if(s == null || s.length() == 0)
+            return 0;
+        
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        
+        int start = 0;
+        int end = 0;
+        
+        while(end < n){
+            char c = s.charAt(end);
+            // If we have already seen this character - get the index of that character and only update the start pointer to that position if its greater than the current start position
+            if(map.containsKey(c))
+                start = Math.max(start, map.get(c));
+            // update the map with index position for each character everytime
+            map.put(c, end+1);
+
+            // Update the maxLen everytime
+            max = Math.max(max, end - start + 1);
+            end++;
+        }
+        return max;
+    }
+}
