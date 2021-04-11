6552aae67a29458479101418e0df23228c181307
Naisargi Patel <naisargip012@gmail.com>
2021-04-10 19:56:35 -0500

    Updated Prob-2

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,26 +1,25 @@
+import java.util.HashMap;
 import java.util.HashSet;
-
+//tc : O(n); n = len of string
+//sc : O(n)
 public class LongestSubstringWithoutRepeatingCharacters {
-    //brute force
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0 || s == null) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i ; j < s.length(); j++) {
-                if (!repeat(s, i, j))
-                    max = Math.max(max, j - i + 1);
+        int max = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)) {
+                start = Math.max(start, map.get(ch));
+
             }
+            map.put(ch, end + 1);
+            max = Math.max(max, end - start + 1);
         }
+
         return max;
-    }
-    private boolean repeat(String s, int i, int j) {
-        HashSet<Character> set= new HashSet<>();
-        for (int in = i; in <= j; in++) {
-            char c = s.charAt(in);
-            if (set.contains(c)) return true;
-            set.add(c);
-        }
-        return false;
     }
 }
