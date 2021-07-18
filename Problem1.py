57df2735c5215e89e1dcc7fb91689a8dea52115a
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-07-18 07:03:06 +0530

    Completed Sprint Strings-1 - Please Review

--- Problem1.py
+++ Problem1.py
@@ -1,10 +1,10 @@
-# Time Complexity : O()
-# Space Complexity : O()
+# Time Complexity : O(n)
+# Space Complexity : O(n)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
 
-#save a map for the count and iterate over the map
+#save a map for the count and iterate over the map then according to the order of order string iterate and append to a string
 class Solution:
     def customSortString(self, order: str, str: str) -> str:
         if len(str) < 2:
