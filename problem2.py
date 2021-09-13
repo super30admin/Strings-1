082d0cef30d1af183887d624807321dc0cd868cd
Yash Naredi <yashnaredi26@gmail.com>
2021-09-13 13:09:08 -0400

    completed Strings-1

---
+++ problem2.py
@@ -0,0 +1,18 @@
+#longest substring without repeating
+#TC: O(N) where n is the len of string
+#SC: O(N)
+class Solution:
+    
+    def lengthOfLongestSubstring(self, s):
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChar[s[i]] = i
+
+        return maxLength
