f572a7a6b83bac698e3a2e6f0bab6eb7c823c90e
LiberoPat <patrick.trias@gmail.com>
2022-02-11 11:32:31 -0800

    completed strings 1

---
+++ p2Longestsubstring.py
@@ -0,0 +1,20 @@
+"""
+time: O(N)
+space: O(N)
+
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res,len(charSet))
+        print(charSet)
+        return res
