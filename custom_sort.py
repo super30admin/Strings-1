6a12a58d25e2a249a6c06ea7743a56d769459720
shwetaterkar <81641786+shwetaterkar@users.noreply.github.com>
2022-03-22 16:59:10 -0400

    Create custom_sort.py

---
+++ custom_sort.py
@@ -0,0 +1,26 @@
+from collections import Counter
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        s_n=Counter(s)
+        result = ""        
+        for c in order:
+            if c in s_n:
+                cnt = s_n.get(c)
+                while cnt !=0:
+                    result=result+c  
+                    cnt-=1
+                del s_n[c]
+        print(result)
+        print(s_n) 
+        
+        # iterate through remaining keys in hashmap
+        for t in s_n:
+            print(t)
+            cl=s_n.get(t)
+            while cl!= 0:
+                result=result+t
+                cl-=1
+            
+            
+        print(result)
+        return result
