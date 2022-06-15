93c649b3e52c51df314bb18f44970bb2adeba496
Charishma090619999 <93498653+Charishma090619999@users.noreply.github.com>
2022-06-15 17:49:12 -0500

    Update longestSubString.py

--- longestSubString.py
+++ longestSubString.py
@@ -12,3 +12,20 @@ class Solution:
             st.add(s[r])
             res=max(res,len(st))
         return res
+    
+    
+    
+    class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        slow=0
+        dict1={}
+        maxi=0
+        for i in range(len(s)):
+            if s[i] in dict1:
+                slow=max(slow,dict1[s[i]]+1)
+            dict1[s[i]]=i
+            
+            maxi=max(i-slow+1,maxi)
+        return maxi
+            
+        
