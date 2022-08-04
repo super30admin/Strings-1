14fb0a1587dc60ded7866c1a675e593717e59eae
SwatiKedarAgarwal <sxa190153@utdallas.edu>
2022-08-04 18:30:49 -0500

    Strings 1 exercise

---
+++ LongestStringWithoutRepeating.java
@@ -0,0 +1,24 @@
+// Time Complexity : O(N)
+// Space Complexity : O(Min(M,N))
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); 
+        
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
