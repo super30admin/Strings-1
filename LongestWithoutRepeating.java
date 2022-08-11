b617271596275051fc12451ca339ef09a3e938f0
Payal Bhandari <payalbhandari@Payals-MacBook-Air-2.local>
2022-08-11 00:00:25 -0700

    Completed Strings-1

---
+++ LongestWithoutRepeating.java
@@ -0,0 +1,25 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1).
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0) return 0;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        int slow = 0;
+        int max = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                slow = Math.max(slow, map.get(c));
+            }
+            map.put(c, i + 1);
+            max = Math.max(max, i - slow + 1); 
+            
+        }
+        
+        return max; 
+    }
+}
