2148708c454f64898e0398599489aee2e689cc39
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-21 21:10:49 -0500

    Completed Sprint Strings-1

---
+++ longest_substring.java
@@ -0,0 +1,23 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+        int max=0;
+        int pos = 0;
+        for (int i=0; i<s.length(); i++){
+            if (hm.containsKey(s.charAt(i))){
+                pos = Math.max(pos,hm.get(s.charAt(i))+1);
+            }
+            hm.put(s.charAt(i),i);
+            max = Math.max(max,i-pos+1);
+        }
+        return max;
+    }
+}
