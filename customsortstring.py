f3a34c567aa6814228812f345688f72cfc43176f
amreenmahenaz <113376911+amreenmahenaz@users.noreply.github.com>
2023-07-22 20:48:11 -0700

    Files added

---
+++ customsortstring.py
@@ -0,0 +1,8 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        ans = ""
+        for i in order:
+            if i in s:
+                ans += (i*s.count(i))
+                s = s.replace(i,'')
+        return ans+s
