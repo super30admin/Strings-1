37336d683d82d7e753da96c9def2ff8893352b31
Syed Ammad Hassan <ammad13k@gmail.com>
2024-01-10 19:15:18 -0600

    updated comments

--- Problem2.java
+++ Problem2.java
@@ -7,7 +7,7 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
 
     Set<Character> set = new HashSet<>();
-        int max=0; // do not take integer.min, as the min value of the length can be 0 , otherwise it gives problem when string length of 0 is encountered
+        int max=0; 
         int start=0,end=0;
 
         while(end<s.length()){
@@ -23,7 +23,7 @@ class Solution {
                 set.remove(s.charAt(start));
                 ++start;
                 }
-                //remember, since we removed the character at the end, we wil not move our end pointer in advance, doing so will result in not recording the chaharacter end is pointing to and will be a mistake
+
             }
             
         }
