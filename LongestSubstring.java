d4a64eef60672db106509ea60611f6489a2d5263
Dishant Patel <dishant@dishants-air.lan>
2024-02-07 23:10:59 -0600

    Strings-1 done

---
+++ LongestSubstring.java
@@ -0,0 +1,20 @@
+//TC = O(n)
+//SC = O(1)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int slow = 0;
+        int max = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(map.containsKey(c))
+            {
+                slow = Math.max(slow, map.get(c) + 1);
+            }
+            map.put(c,i);
+            max = Math.max(max, i - slow + 1);
+        }
+        return max;
+    }
+}
